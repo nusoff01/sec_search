01521fbbac93e83a3afee8f549f71d062cc9819f
Nick Usoff <nusoff@Nicks-MacBook-Pro.local>
2016-05-19 09:59:16 -0400

    added functionality to search for which codes are included in the filing, and restricted search to just 8-K filings

--- extract_8k_html.py
+++ extract_8k_html.py
@@ -45,19 +45,22 @@ def find_classification (cik_page):
 # create_CIK_URL: from a CIK, creates and returns the corresponding search url
 
 def create_CIK_URL (cik_num):
-	return sec_base + "cgi-bin/browse-edgar?CIK=" + cik_num + "&owner=exclude&action=getcompany&Find=Search"
+	return sec_base + "cgi-bin/browse-edgar?action=getcompany&CIK=" + cik_num + "&type=8-K%25&dateb=&owner=exclude&start=0&count=100"
 
 
+# boolean function which checks to see if there is a next page of filings for a company
+def next_page_present (cik_page):
+	soup = BeautifulSoup(cik_page, "html.parser")
+	table = soup.findAll('table')[1]
+	print(table)
+	return 0
 
-
-print(create_CIK_URL("10254"))
-search_CIK("10254")
 
 
 
 # Go through each line in templist and use as a CIK
 
-with open('templist.txt') as f:
+with open('templist_short.txt') as f:
     lines = f.read().splitlines()
 
 cik_pages = []
@@ -73,9 +76,27 @@ for line in lines:
 
 
 url = "http://www.samhsa.gov/data/NSDUH/2k10State/NSDUHsae2010/NSDUHsaeAppC2010.htm"
-soup = BeautifulSoup(urllib2.urlopen(url).read())
+soup = BeautifulSoup(cik_pages[0], "html.parser")
 
-for row in soup.findAll('table')[0].tbody.findAll('tr'):
-    first_column = row.findAll('th')[0].contents
-    third_column = row.findAll('td')[2].contents
-    print first_column, third_column
+# for row in soup.findAll('table')[2].findAll('tr'):
+# 	print(row)
+	# if(row.find("8-K") != -1):
+	# 	print(row)
+
+soupfind = soup.findAll('table')[2].findAll('tr')
+print(len(soupfind))
+
+countFilings = 0
+for row in soupfind:
+	try:
+		typeTDStr = str(row.findAll('td')[2])
+		if(typeTDStr.find("2.02") > -1 or typeTDStr.find("2.05") > -1):
+			print(typeTDStr)
+			countFilings += 1
+	except:
+		pass
+
+print("2.02 count: " + str(countFilings))
+# ncext_page_present(cik_pages[0])
+
+    
