fe0997a0c1c4fa49b441a2a07213e3f8253576db
Nick Usoff <nicholas.usoff@tufts.edu>
2016-05-21 11:45:03 -0400

    everything loads in batches now, from CIK pages to filing pages to filings themselves. Also added timing output. Looks like it's about 3 times as fast as it was when everything was loaded synchronously

--- extract_8k_html.py
+++ extract_8k_html.py
@@ -14,6 +14,8 @@
 from bs4 import BeautifulSoup
 import os
 import urllib2
+from datetime import datetime
+import grequests
 
 
 ##############################################################################
@@ -21,77 +23,96 @@ import urllib2
 ##############################################################################
 
 sec_base = "http://www.sec.gov/"
-
+startTime = datetime.now()
 
 ##############################################################################
 # Functions
 ##############################################################################
 
-# search_CIK: takes in a CIK number, and returns the raw page of the search
-#             result in a tuple with the CIK number
+# from a cik_page, return a list of filings with the correct
 
-def search_CIK (cik_num, cik_start):
-	cik_url = create_CIK_URL(cik_num, cik_start)
-	# print("searching: " + cik_url)
-	response = urllib2.urlopen(cik_url)
-	cik_html = response.read()
-	return (cik, cik_html)
-
-
-# search_documents: takes in a cik_num and returns a list of document URLs for
-#                   any document which contains either a 2.01 or 2.05
-
-def find_classification (cik_num):
-	cik_start = 0
-	cont_true = True
-	count_filings = 0
+def find_filings (cik_page):
 	filing_links = []
-	while cont_true:
-		cik_page = search_CIK(cik_num, cik_start)
-		soup = BeautifulSoup(cik_page[1], "html.parser")
-		try:
-			soupfind = soup.findAll('table')[2].findAll('tr')
+	count_filings = 0
+	soup = BeautifulSoup(cik_page, "html.parser")
+	try:
+		soupfind = soup.findAll('table')[2].findAll('tr')
+		for row in soupfind:
+			try:
+				typeTDStr = str(row.findAll('td')[2])
+				if(typeTDStr.find("2.01") > -1 or typeTDStr.find("2.05") > -1):
+					count_filings += 1
+					filing_links.append(row.findAll('a')[0]['href'])
+			except:
+				pass
+	except:
+		pass
+	return filing_links
 
-			for row in soupfind:
-				try:
-					typeTDStr = str(row.findAll('td')[2])
-					if(typeTDStr.find("2.01") > -1 or typeTDStr.find("2.05") > -1):
-						count_filings += 1
-						filing_links.append(row.findAll('a')[0]['href'])
-				except:
-					pass
-		except:
-			pass
-		cont_true = next_page_present(cik_page[1])
+# ciks_to_classifications: given a list of ciks, return a list of tuples with
+#                          a cik and a link to a filing associated with that 
+#						   cik
+
+def ciks_to_classifications (cik_list):
+	cik_urls = []
+	cik_start = 0
+	filing_links_list = []
+
+	while len(cik_list) > 0 : # only proceed if there are urls left to process
+		for cik in cik_list:
+			cik_urls.append(create_CIK_URL(cik, cik_start))
+		rs = (grequests.get(cik_url) for cik_url in cik_urls)
+		responses = grequests.map(rs)
+
+		cik_content_list = content_zipper(cik_list, responses)
+		cik_list = []
+		cik_urls = []
+		for cik_content in cik_content_list:
+			filing_links = find_filings(cik_content[1])
+			for filing_link in filing_links:
+				filing_links_list.append((cik_content[0], str(filing_link)))
+			if next_page_present(cik_content[1]):
+				cik_list.append(cik_content[0])
+
 		cik_start += 100
-	print(str(cik_num) + ": " + str(count_filings))
-	return (filing_links, cik_num)
+	print("loaded CIK pages. Total time elapsed: " + 
+		  str(datetime.now() - startTime))
+	return filing_links_list
 
 
-# extract_document: from a link to a filing, find the corresponding document
-#		            url and path where it should go
+# extract_documents: from a list of links to filings, find the corresponding
+#                    document urls and path where it should go
 
-def extract_document(filing_link, cik):
-	filing_url = sec_base + filing_link
-	print("filing_url: " + filing_url)
-	response = urllib2.urlopen(filing_url)
-	filing_html = response.read()
-	soup = BeautifulSoup(filing_html, "html.parser")
-	filing_rows = soup.findAll('table')[0].findAll('tr')
-	for row in filing_rows:
-		if(str(row).find('<td scope="row">8-K') != -1):
-			# print(row)
-			doc_url = sec_base + str(row.findAll('a')[0]['href'])
-			last_slash = doc_url.rfind('/')
-			trunc_url = doc_url[:last_slash]
-			filing_id = trunc_url[trunc_url.rfind('/'):]
+def extract_documents(filing_links_list):
+	filing_urls = []
+	ciks = []
+	for filing_link in filing_links_list:
+		ciks.append(filing_link[0])
+		filing_urls.append(sec_base + filing_link[1])
+	rs = (grequests.get(filing_url) for filing_url in filing_urls)
+	responses = grequests.map(rs)
+	print("loaded filing pages. Total time elapsed: " + 
+		  str(datetime.now() - startTime))
+	cik_filing_list = content_zipper(ciks, responses)
+	
+	doc_path_list = []
+	for cik_filing in cik_filing_list:
+		soup = BeautifulSoup(cik_filing[1], "html.parser")
+		filing_rows = soup.findAll('table')[0].findAll('tr')
+		for row in filing_rows:
+			if(str(row).find('<td scope="row">8-K') != -1):
+				doc_url = sec_base + str(row.findAll('a')[0]['href'])
+				last_slash = doc_url.rfind('/')
+				trunc_url = doc_url[:last_slash]
+				filing_id = trunc_url[trunc_url.rfind('/'):]
 
-			# file_from_url(doc_url, ("output/"+ cik + "/" + filing_id + "_"
-			# 	          + doc_url[last_slash + 1:]))
-			return (doc_url, ("output/"+ cik + "/" + filing_id + "_"
-				          + doc_url[last_slash + 1:]))
-	return
-
+				# file_from_url(doc_url, ("output/"+ cik + "/" + filing_id + "_"
+				# 	          + doc_url[last_slash + 1:]))
+				doc_path_list.append((doc_url, ("output/"+ cik_filing[0] + "/" +
+					                  filing_id + "_"+ doc_url[last_slash 
+					                  + 1:])))
+				continue
+	return doc_path_list 
 
 ##############################################################################
 # Helper Functions  
@@ -112,39 +133,17 @@ def create_CIK_URL (cik_num, startNum):
 def next_page_present (cik_page):
 	soup = BeautifulSoup(cik_page, "html.parser")
 	table = soup.findAll('table')[1]
-	return (str(table).find("Next 100")) != -1
+	return (str(table).find("Next 100") != -1)
+
+# content_zipper: given a list of responses and another list, zip the other
+#                 list with the content of each response
+def content_zipper (other_list, responses):
+	content_unzipped = []
+	for response in responses:
+		content_unzipped.append(response.content)
+	return zip(other_list, content_unzipped)
 
 
-# file_from_url: takes in a url and a location, and writes to the location 
-#                the raw HTML at whatever the url is.
-
-def file_from_url (url, path):
-	response = urllib2.urlopen(url)
-	html = response.read()
-
-	if not os.path.exists(os.path.dirname(path)):
-	    try:
-	        os.makedirs(os.path.dirname(path))
-	    except OSError as exc: # Guard against race condition
-	        if exc.errno != errno.EEXIST:
-	            raise
-	with open(path, "w") as f:
-	    f.write(html)
-	return
-
-	
-##############################################################################
-# Testing Functions
-##############################################################################
-
-def test_file_from_url ():
-	url = "https://www.sec.gov/Archives/edgar/data/1041859/000114420408067808/v134037_8k.htm"
-	file_from_url(url, "testOutput/1.html" )
-	return
-
-##############################################################################
-# Exectution 
-##############################################################################
 
 # write_to_file: takes in a file and a path and writes the file to the path
 
@@ -159,33 +158,33 @@ def write_to_file(file, path):
 	    f.write(file)
 	return
 
+##############################################################################
+# Testing Functions
+##############################################################################
+
+
+
+##############################################################################
+# Exectution 
+##############################################################################
 
 # Go through each line in templist and use as a CIK
 
 with open('templist.txt') as f:
     lines = f.read().splitlines()
 
-cik_pages = []
-
-filing_links_list = []
-for cik in lines:
-	filing_links_list.append(find_classification(cik))
 
 
-url_path_list = []
-for company in filing_links_list:
-	for filing in company[0]:
-	 	url_path_list.append(extract_document(filing, company[1]))
+filing_links_list = ciks_to_classifications(lines)
+url_path_list = extract_documents(filing_links_list)
 
 
-
-
-
-import grequests
 rs = (grequests.get(u[0]) for u in url_path_list)
 
 path_list = zip(*url_path_list)[1]
 responses = grequests.map(rs)
+print("loaded raw filings. Total time elapsed: " + 
+		  str(datetime.now() - startTime))
 
 url_unzipped = []
 content_unzipped = []
@@ -199,11 +198,8 @@ for res_path in response_path_list:
 	write_to_file(res_path[0], res_path[1])
 
 
-
-
-
-
-
+print("Wrote files locally. Total time elapsed: " + 
+	  str(datetime.now() - startTime))
 
 
 
